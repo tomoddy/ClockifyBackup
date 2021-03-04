0b8f1987c4a485f96380fd926cbe01bc42babc26
Tom Oddy <tom_oddy@live.co.uk>
2021-03-04 14:57:38 +0000

    Added additional argument checks

--- ClockifyBackup/ClockifyBackup/Program.cs
+++ ClockifyBackup/ClockifyBackup/Program.cs
@@ -10,20 +10,20 @@ namespace ClockifyBackup
         static void Main(string[] args)
         {
             if (args.Length != ARGS_LENGTH)
-            {
                 Console.WriteLine($"Expected {ARGS_LENGTH} arguments, got {args.Length}");
-            }
             else
             {
                 string args0 = Path.GetDirectoryName(args[0]);
                 bool args2 = int.TryParse(args[2], out int exportType);
                 bool args3 = int.TryParse(args[3], out int dateRange);
 
-                if (!string.IsNullOrEmpty(args0) && args2 && args3)
+                if (!string.IsNullOrEmpty(args0) && args2 && exportType >= 0 && exportType < 3 && args3)
                 {
                     Client client = new Client(args[0], args[1], exportType);
                     client.Request(DateTime.Now.AddDays(-dateRange), DateTime.Now);
                 }
+                else
+                    Console.WriteLine("Invalid arguments");
             }
         }
     }
