45159504c79496acc26c38649f77cda7834d6049
Tom Oddy <tom_oddy@live.co.uk>
2021-03-04 12:18:36 +0000

    Add basic project files

---
+++ ClockifyBackup/ClockifyBackup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClockifyBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
