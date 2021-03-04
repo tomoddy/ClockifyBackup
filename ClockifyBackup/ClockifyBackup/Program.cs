d0e85e0b219d92653ef253bccbc8ad74a4f2cdd3
Tom Oddy <tom_oddy@live.co.uk>
2021-03-04 12:27:32 +0000

    Added basic rest client example

--- ClockifyBackup/ClockifyBackup/Program.cs
+++ ClockifyBackup/ClockifyBackup/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestSharp;
+using System;
+using System.Configuration;
 
 namespace ClockifyBackup
 {
@@ -6,7 +8,20 @@ namespace ClockifyBackup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string workspaceId = ConfigurationManager.AppSettings.Get("WorkspaceId");
+            RestClient client = new RestClient($"https://reports.api.clockify.me/v1/workspaces/{workspaceId}/reports/detailed")
+            {
+                Timeout = -1
+            };
+
+            RestRequest request = new RestRequest(Method.POST);
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("x-api-key", ConfigurationManager.AppSettings.Get("ApiKey"));
+            request.AddParameter("application/json", "{\r\n  \"dateRangeStart\": \"2021-01-01T00:00:00.000\",\r\n  \"dateRangeEnd\": \"2021-01-05T23:59:59.000\",\r\n  \"detailedFilter\": {\r\n    \"page\": 1,\r\n    \"pageSize\": 50\r\n  },\r\n  \"exportType\": \"JSON\"\r\n}", ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
         }
     }
 }
