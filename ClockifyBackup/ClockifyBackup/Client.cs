aaec10a3c40de3a317f7ed4796b88888210436f9
Tom Oddy <tom_oddy@live.co.uk>
2021-03-04 15:13:36 +0000

    Added documentation and export library

--- ClockifyBackup/ClockifyBackup/Client.cs
+++ ClockifyBackup/ClockifyBackup/Client.cs
@@ -6,8 +6,12 @@ using System.IO;
 
 namespace ClockifyBackup
 {
+    /// <summary>
+    /// Client class
+    /// </summary>
     class Client
     {
+        // Enums
         public enum ExportTypes
         {
             JSON,
@@ -15,12 +19,19 @@ namespace ClockifyBackup
             XLSX
         }
 
+        // Properties
         public string ApiKey { get; set; }
         public RestClient RestClient { get; set; }
         public string ExportPath { get; set; }
         public string ExportName { get; set; }
         public ExportTypes ExportType { get; set; }
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="path">Output path</param>
+        /// <param name="name">Output file name</param>
+        /// <param name="type">Output file type</param>
         public Client(string path, string name, int type)
         {
             ApiKey = ConfigurationManager.AppSettings.Get("ApiKey");
@@ -30,17 +41,31 @@ namespace ClockifyBackup
             ExportType = (ExportTypes)type;
         }
 
+        /// <summary>
+        /// Make request
+        /// </summary>
+        /// <param name="startDate">Start date</param>
+        /// <param name="endDate">End date</param>
         public void Request(DateTime startDate, DateTime endDate)
-        { 
+        {
+            // Create request body
+            Console.WriteLine("Creating request body");
             RequestBodyObj requestBody = new RequestBodyObj(startDate, endDate, ExportType.ToString());
 
+            // Create requst
+            Console.WriteLine("Creating request");
             RestRequest request = new RestRequest(Method.POST);
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("x-api-key", ApiKey);
             request.AddParameter("application/json", JsonConvert.SerializeObject(requestBody), ParameterType.RequestBody);
 
+            // Execute request
+            Console.WriteLine("Executing request");
             IRestResponse response = RestClient.Execute(request);
-            File.WriteAllBytes($"{ExportPath}/{ExportName}{DateTime.Now.ToString($"-yyyy-MM-dd-HH-mm-ss")}.{ExportType}", response.RawBytes);
+
+            // Write result to file
+            Console.WriteLine("Writing to file");
+            File.WriteAllBytes($"{ExportPath}/{ExportName}{DateTime.Now.ToString($"-yyyy-MM-dd-HH-mm-ss")}.{ExportType.ToString().ToLower()}", response.RawBytes);
         }
     }
 }
--- ClockifyBackup/ClockifyBackup/Program.cs
+++ ClockifyBackup/ClockifyBackup/Program.cs
@@ -3,28 +3,41 @@ using System.IO;
 
 namespace ClockifyBackup
 {
+    /// <summary>
+    /// Main class
+    /// </summary>
     class Program
     {
-        const int ARGS_LENGTH = 4;
-
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args">Comand line arguments</param>
         static void Main(string[] args)
         {
-            if (args.Length != ARGS_LENGTH)
-                Console.WriteLine($"Expected {ARGS_LENGTH} arguments, got {args.Length}");
+            if (args.Length != 4)
+                // Invalid number of arguments
+                Console.WriteLine($"Expected 4 arguments, got {args.Length}");
             else
             {
+                // Get argument check values
                 string args0 = Path.GetDirectoryName(args[0]);
                 bool args2 = int.TryParse(args[2], out int exportType);
                 bool args3 = int.TryParse(args[3], out int dateRange);
 
+                // Check argument values
                 if (!string.IsNullOrEmpty(args0) && args2 && exportType >= 0 && exportType < 3 && args3)
                 {
+                    // Create client and make request
                     Client client = new Client(args[0], args[1], exportType);
                     client.Request(DateTime.Now.AddDays(-dateRange), DateTime.Now);
                 }
                 else
+                    // Print error message
                     Console.WriteLine("Invalid arguments");
             }
+
+            // Print success message
+            Console.WriteLine("SUCCESS");
         }
     }
 }
